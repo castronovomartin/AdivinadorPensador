0fdbb86f420da2efa6099a5933e513255637f007
Tincho Castronovo <tinchocastronovo@gmail.com>
2019-05-29 19:52:54 -0300

    Se creó la clase NumeroElegido
    aquí se guardará el número elegido tanto por la pc como por el usuario en cada caso

---
+++ src/main/java/Adivinador/Pensador/entity/NumeroElegido.java
@@ -0,0 +1,53 @@
+package Adivinador.Pensador.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "numeros_elegidos")
+
+public class NumeroElegido {
+	
+	//ATRIBUTOS
+	@Id
+	@Column(name = "id", unique = true, nullable = false)
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
+	@GeneratedValue(generator = "uuid")
+	private String id;
+		
+	@Column(name = "numero")
+	private String numero;
+
+	//CONSTRUCTOR
+	public NumeroElegido(String numero) {
+		super();
+		this.numero = numero;
+	}
+	
+	public NumeroElegido() {
+		super();
+	}
+
+	//GET AND SET
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+	
+}
