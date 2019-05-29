a9e30a59683d1257074d38ce10b76c5ce8d14d50
Tincho Castronovo <tinchocastronovo@gmail.com>
2019-05-29 19:16:41 -0300

    Creación y configuración inicial del proyecto

---
+++ src/main/java/Adivinador/Pensador/PensadorApplication.java
@@ -0,0 +1,13 @@
+package Adivinador.Pensador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PensadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PensadorApplication.class, args);
+	}
+
+}
